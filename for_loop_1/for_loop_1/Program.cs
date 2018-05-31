7e51a97c10cba2e4fc1988ede247de6137e63351
Christian Gentry <christian.gentry@MBP-ChristianGentry.local>
2018-05-31 17:46:08 -0500

    1

--- for_loop_1/for_loop_1/Program.cs
+++ for_loop_1/for_loop_1/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace for_loop_1
 {
+
+    //a simple program to display the words Hello World
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> names = List new { "christian", "zachery", "chantel", "jj" };
+            Console.WriteLine(names[0]);
         }
     }
 }
---
+++ for_loop_2/for_loop_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace for_loop_2
+{
+
+    //a simple program to display the words Hello World
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string> { "christian", "zachery", "chantel", "jj" };
+            //Console.WriteLine(names[count]);
+
+            for (int i = 0; i < names.Count; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+
+
+        }
+    }
+}
